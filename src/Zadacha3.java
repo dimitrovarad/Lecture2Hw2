93ba8746b5ff73434bfc55c47a8f64a1cc9b4110
Radina Valentinova Dimitrova <Radina.Dimitrova@allocatesoftware.com>
2018-07-25 14:44:39 +0300

    Lecture 2 java examples

---
+++ src/Zadacha3.java
@@ -0,0 +1,12 @@
+public class Zadacha3 {
+    //Задача 3: Да се изведат на екрана всички нечетни числа от -10 до 10
+
+    public static void main(String[] args) {
+        for (int x = -10; x < 10; x++) {
+            if (x % 2 != 0) {
+                System.out.println(x);
+            }
+        }
+    }
+
+}
---
+++ src/Zadacha5.java
@@ -0,0 +1,9 @@
+public class Zadacha5 {
+    //Задача 5: Да се въведат от потребителя 2 числа. И да се изведат на екрана всички числа от по-малкото до по-голямото.
+    public static void main(String[] args) {
+        for (int x = 15; x <= 20; x++) {
+            System.out.println(x);
+        }
+    }
+
+}
